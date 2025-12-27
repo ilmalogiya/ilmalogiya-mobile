c5700be240b46e35b43da9b475d3febc66205df9
Bobur <boburotaboyev0@gmail.com>
2025-12-27 15:32:31 +0500

    first commit

---
+++ android/app/src/main/kotlin/uz/afsona_group/ilmalogiya/MainActivity.kt
@@ -0,0 +1,5 @@
+package uz.afsona_group.ilmalogiya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main(List<String> args) {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+  }
+}
